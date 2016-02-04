a741b547ab3c3c4a20622c16063b431d8e012aff
Clark Groce <cgroce@cgroce-ltm.internal.salesforce.com>
2016-02-03 23:46:32 -0500

    optimized adding background pizza images

--- views/js/main.js
+++ views/js/main.js
@@ -528,9 +528,9 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizza78x100.png";
     elem.style.height = "100px";
-    elem.style.width = "73.333px";
+    elem.style.width = "78px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
